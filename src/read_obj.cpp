1d574fe48cb08135432f8149f7b79e3032652460
Ilshat Fatkhullin <ilshat.fatkhullin@gmail.com>
2020-02-19 18:27:34 +0300

    Added read obj lab.

--- src/read_obj.cpp
+++ src/read_obj.cpp
@@ -7,7 +7,7 @@
 #include <algorithm>
 
 
-ObjParser::ObjParser(std::string filename):
+ObjParser::ObjParser(std::string filename) :
 	filename(filename)
 {
 }
@@ -20,7 +20,55 @@ ObjParser::~ObjParser()
 
 int ObjParser::Parse()
 {
-	
+	std::ifstream obj_file(filename, std::ifstream::in);
+	if (obj_file.fail())
+	{
+		return 1;
+	}
+
+	std::string line;
+
+	while (std::getline(obj_file, line))
+	{
+		if (line.rfind("v ", 0) == 0)
+		{
+			auto vertexData = Split(line, ' ');
+			float4 vertex{ 0, 0, 0, 1 };
+			for (int i = 1; i < vertexData.size(); i++)
+			{
+				vertex[i - 1] = std::stof(vertexData.at(i));
+			}
+			vertexes.push_back(vertex);
+		}
+		else if (line.rfind("f ", 0) == 0)
+		{
+			auto facesData = Split(line, ' ');
+			std::vector<int> indecies;
+			for (int i = 1; i < facesData.size(); i++)
+			{
+				std::vector<std::string> indexData = Split(facesData.at(i), '/');
+				int index = std::stoi(indexData.at(0));
+				if (index > 0)
+				{
+					index -= 1;
+				}
+				else {
+					index += vertexes.size();
+				}
+				indecies.push_back(index);
+			}
+			for (int i = 0; i < indecies.size() - 2; i++)
+			{
+				face face;
+				face.vertexes[0] = vertexes[indecies.at(0)];
+				face.vertexes[1] = vertexes[indecies.at(i + 1)];
+				face.vertexes[2] = vertexes[indecies.at(i + 2)];
+				faces.push_back(face);
+			}
+
+		}
+	}
+
 	return 0;
 }
 
@@ -42,7 +90,7 @@ std::vector<std::string> ObjParser::Split(const std::string& s, char delimiter)
 }
 
 
-ReadObj::ReadObj(unsigned short width, unsigned short height, std::string obj_file): LineDrawing(width, height)
+ReadObj::ReadObj(unsigned short width, unsigned short height, std::string obj_file) : LineDrawing(width, height)
 {
 	parser = new ObjParser(obj_file);
 }
@@ -54,6 +102,28 @@ ReadObj::~ReadObj()
 
 void ReadObj::DrawScene()
 {
+	parser->Parse();
 
+	float x_center = width / 2.0;
+	float y_center = height / 2.0;
+	float radius = std::min(x_center, y_center) - 40.0;
+
+	for (auto face : parser->GetFaces()) {
+		DrawLine(static_cast<unsigned short>(x_center + radius * face.vertexes[0].x),
+			static_cast<unsigned short>(y_center + radius * face.vertexes[0].y),
+			static_cast<unsigned short>(x_center + radius * face.vertexes[1].x),
+			static_cast<unsigned short>(y_center + radius * face.vertexes[1].y),
+			color(255, 0, 0));
+		DrawLine(static_cast<unsigned short>(x_center + radius * face.vertexes[1].x),
+			static_cast<unsigned short>(y_center + radius * face.vertexes[1].y),
+			static_cast<unsigned short>(x_center + radius * face.vertexes[2].x),
+			static_cast<unsigned short>(y_center + radius * face.vertexes[2].y),
+			color(0, 255, 0));
+		DrawLine(static_cast<unsigned short>(x_center + radius * face.vertexes[2].x),
+			static_cast<unsigned short>(y_center + radius * face.vertexes[2].y),
+			static_cast<unsigned short>(x_center + radius * face.vertexes[0].x),
+			static_cast<unsigned short>(y_center + radius * face.vertexes[0].y),
+			color(0, 0, 255));
+	}
 }
 
